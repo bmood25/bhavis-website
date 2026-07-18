3efb59d0d559dbce5d92b326f4149c57bd914985
bmood25 <bcmood25@gmail.com>
2026-07-18 18:02:09 -0400

    Redesign: sky blue/beige palette, bubble font, melting animations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 (function () {
   'use strict';
 
+  /* Melting wavy hero title */
+  var waveTargets = document.querySelectorAll('.wavy-text');
+
+  waveTargets.forEach(function (el) {
+    var text = el.textContent;
+    el.setAttribute('aria-label', text);
+    el.innerHTML = '';
+
+    text.split('').forEach(function (char, i) {
+      var span = document.createElement('span');
+      span.className = 'letter' + (char === ' ' ? ' is-space' : '');
+      span.style.setProperty('--i', i);
+      span.setAttribute('aria-hidden', 'true');
+      span.textContent = char === ' ' ? ' ' : char;
+      el.appendChild(span);
+    });
+  });
+
   /* Scroll reveal */
   var revealEls = document.querySelectorAll('.reveal');
 
